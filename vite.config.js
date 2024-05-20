b6456c84b7bdcb4f6937d5823f31afb1d92c3281
Juliusz Zolich <115212932+JuliuszZolich@users.noreply.github.com>
2024-05-20 19:32:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/projekt-aplikacja/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    ghPages()
+  ],
 })
