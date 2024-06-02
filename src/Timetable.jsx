f645a24c836913b3dd3ee862bd8a4f56d9dcf94f
Sebastian <245787@edu.p.lodz.pl>
2024-06-02 20:35:19 +0200

    Dodanie pustej strony Terminarza

---
+++ src/Timetable.jsx
@@ -0,0 +1,10 @@
+import TopBarAndSideMenu from "./TopBarAndSideMenu.jsx";
+const Timetable = () => {
+    return(
+        <>
+            {TopBarAndSideMenu()}
+        </>
+    )
+}
+
+export default Timetable;
