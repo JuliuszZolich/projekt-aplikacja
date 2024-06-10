20c500488ce4ec3b7b34b69b2a37952097ad05f2
Juliusz Zolich <245971@edu.p.lodz.pl>
2024-06-10 21:42:56 +0200

    Zalążek funkcjonalności map

--- src/MapResult.jsx
+++ src/MapResult.jsx
@@ -3,35 +3,59 @@ import TopBarAndSideMenu from "./TopBarAndSideMenu.jsx";
 import {Link} from "react-router-dom";
 import {useLanguage} from './ChangeLanguage.jsx';
 import findicon from "./assets/find.png"
-import locationimage from "./assets/weeia.jpg"
+import {useEffect, useState} from "react";
+
+async function get_map(builingID, setMap, translation) {
+    let headers = new Headers();
+    headers.append("BuildingID", builingID);
+    headers.append("UserID", -1);
+    headers.append("Action-Type", "GET");
+    const response = await fetch('http://localhost:8001/map/', {
+        method: 'GET',
+        headers: headers,
+    });
+    const data = await response.json();
+    let mapData = Array.isArray(data["map"]) ? data["map"] : [data["map"]];
+    setMap(mapData.map(map => {
+            const image_src = `${map["img"]}`;
+            return (
+                <div className={"map-result-main-content"} key={map["building"]}>
+                    <div className={"map-result-left-content"}>
+                        <div className={"map-result-find"}>
+                            <Link to={"/projekt-aplikacja/mapresult"}>
+                                <img src={findicon} alt="finc-icon" title={"Znajdz"}/>
+                            </Link>
+                            <input type={"text"} name={"find"} placeholder={translation.Map.find}/>
+                        </div>
+                        <div className={"map-result-iframe"}>
+                            <iframe src={map["iframe"]}></iframe>
+                        </div>
+                    </div>
+                    <div className={"map-result-right-content"}>
+                        <div className={"map-result-location-image"}>
+                            <img src={image_src} alt="location-image"/>`
+                        </div>
+                        <div className={"map-result-location-name"}>
+                            <p>{translation.Map.building} {map["building"]} {map["address"]}, {translation.Map.campus} {map["campus"]}</p>
+                            <p>{translation.Map.faculty} {map["facility"]}</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    ));
+}
 
 const MapResult = () => {
     const {t: translation} = useLanguage();
+    const [Map, setMap] = useState([]);
+    useEffect(() => {
+        get_map("A1", setMap, translation).then(() => console.log("Map loaded"));
+    }, []);
     return (
         <>
             {TopBarAndSideMenu()}
-            <div className={"map-result-main-content"}>
-                <div className={"map-result-left-content"}>
-                    <div className={"map-result-find"}>
-                        <Link to={"/projekt-aplikacja/mapresult"}>
-                            <img src={findicon} alt="finc-icon" title={"Znajdz"}/>
-                        </Link>
-                        <input type={"text"} name={"find"} placeholder={translation.Map.find}/>
-                    </div>
-                    <div className={"map-result-iframe"}>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2420.217857163626!2d21.744122993635052!3d52.656042454209754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471f037c7bd4657d%3A0xeedc3814c5cb25d3!2sAgroturystyka%20Na%20Zadupiu!5e0!3m2!1spl!2spl!4v1716817941116!5m2!1spl!2spl"></iframe>
-                    </div>
-                </div>
-                <div className={"map-result-right-content"}>
-                    <div className={"map-result-location-image"}>
-                        <img src={locationimage} alt="location-image"/>
-                    </div>
-                    <div className={"map-result-location-name"}>
-                        <p>{translation.Map.building} A10 ul. Wólczańska 175, {translation.Map.campus} A</p>
-                        <p>{translation.Map.faculty} EEIA, II {translation.Map.floor}</p>
-                    </div>
-                </div>
-            </div>
+            {Map}
         </>
     )
 }
