34feb7f4d0a62a3df9af4f184c620d1249806140
Juliusz Zolich <245971@edu.p.lodz.pl>
2024-06-12 21:03:28 +0200

    Poprawa działania notatek Dodanie wyszukiwania tekstu w tytule i zawartości Refaktoryzacja nazw metod

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -8,21 +8,28 @@ import modifyicon from "./assets/modify.png"
 import {useEffect, useState} from "react";
 import {useCookies} from "react-cookie";
 
+const titleDisplayedSize = 13;
 let currentNoteId = 0;
 
-async function remove_note(noteID, cookies){
+
+async function removeNote(noteID, cookies, setNotes) {
     let headers = new Headers();
     headers.append("UserID", cookies['userID']);
     headers.append("Action-Type", "DELETE");
     headers.append("Note-ID", noteID);
-    await fetch("http://localhost:8001/notes", {
+    const response = await fetch("http://localhost:8001/notes", {
         method: "GET",
         headers: headers,
     });
-    window.location.reload();
+
+    if (response.ok) {
+        setNotes(prevNotes => prevNotes.filter(note => note.key !== noteID));
+    } else {
+        console.error(`Failed to delete note with ID: ${noteID}`);
+    }
 }
 
-async function get_notes(user_id, setNotes, cookies) {
+async function getNotes(user_id, setNotes, cookies) {
     let headers = new Headers();
     headers.append("UserID", user_id);
     headers.append("Action-Type", "GET");
@@ -36,31 +43,37 @@ async function get_notes(user_id, setNotes, cookies) {
     }
     let data = await response.json();
 
-    function show_note_widow(note) {
-        if(document.getElementsByClassName("delete-window")[0].style.display !== "none") return;
+    function showNoteWindow(note) {
+        if (document.getElementsByClassName("delete-window")[0].style.display !== "none") return;
         currentNoteId = note["id"];
         document.getElementsByClassName("note-window")[0].style.display = "block";
         document.getElementsByClassName("note-window-top-bar-title")[0].textContent = note["title"];
-        document.getElementsByClassName("note-window-text")[0].innerHTML = note["content"].replace(new RegExp("\\n", "g"),"<br />");
+        document.getElementsByClassName("note-window-text")[0].innerHTML = note["content"].replace(new RegExp("\\n", "g"), "<br />");
     }
 
     setNotes(data["notes"].map(note => {
             return (
-                <div className={'note'} key={note["id"]}  onClick={() => {
-                    show_note_widow(note);
+                <div className={'note'} key={note["id"]} onClick={() => {
+                    showNoteWindow(note);
                 }}>
                     <div className={'note-title'}>
-                        {note["title"]}
+                        {(() => {
+                            if (note["title"].length < titleDisplayedSize) return note["title"];
+                            else return note["title"].substring(0, titleDisplayedSize).trim() + "...";
+                        })()
+                        }
+                        <div id={"fulltitle"} style={{"display": "none"}}>{note["title"]}</div>
                     </div>
                     <div className={'note-content'}
-                         dangerouslySetInnerHTML={{ __html: note["content"].replace(new RegExp("\\n", "g"),"<br />") }
-                    }>
+                         dangerouslySetInnerHTML={{__html: note["content"].replace(new RegExp("\\n", "g"), "<br />")}
+                         }>
                     </div>
                     <div className={'note-delete'} onClick={() => {
-                        if(document.getElementsByClassName("delete-window")[0].style.display === "none") {
+                        if (document.getElementsByClassName("delete-window")[0].style.display === "none") {
                             document.getElementsByClassName("delete-window")[0].style.display = "block";
                             document.getElementsByClassName("delete-window-bottom-bar-delete")[0].onclick = () => {
-                                remove_note(note["id"], cookies);
+                                removeNote(note["id"], cookies, setNotes);
+                                document.getElementsByClassName("delete-window")[0].style.display = "none";
                             }
                             document.getElementsByClassName("delete-window-bottom-bar-keep")[0].onclick = () => {
                                 document.getElementsByClassName("delete-window")[0].style.display = "none";
@@ -78,12 +91,12 @@ async function get_notes(user_id, setNotes, cookies) {
     ));
 }
 
-async function modify_note(user_id, note_id, title, content) {
+async function modifyNote(user_id, note_id, title, content, setNotes) {
     let headers = new Headers();
     headers.append("UserID", user_id);
     headers.append("Action-Type", "UPDATE");
     headers.append("Note-ID", note_id);
-    await fetch("http://localhost:8001/notes", {
+    const response = await fetch("http://localhost:8001/notes", {
         method: "PUT",
         headers: headers,
         body: JSON.stringify({
@@ -91,11 +104,16 @@ async function modify_note(user_id, note_id, title, content) {
             "content": content
         })
     });
-    window.location.reload();
+
+    if (response.ok) {
+        setNotes(prevNotes => prevNotes.map(note => note.key === note_id ? {...note, title, content} : note));
+    } else {
+        console.error(`Failed to modify note with ID: ${note_id}`);
+    }
 }
 
-async function add_note(user_id, title, content) {
-let headers = new Headers();
+async function addNote(user_id, title, content, setNotes, cookies) {
+    let headers = new Headers();
     headers.append("UserID", user_id);
     headers.append("Action-Type", "POST");
     await fetch("http://localhost:8001/notes", {
@@ -105,27 +123,38 @@ let headers = new Headers();
             "title": title,
             "content": content
         })
+    }).then(() => {
+        getNotes(user_id, setNotes, cookies);
     });
-    window.location.reload();
 }
 
 const Notes = () => {
     const [notes, setNotes] = useState([]);
     const [cookies] = useCookies([]);
+    const [searchTerm, setSearchTerm] = useState('');
     useEffect(() => {
-        get_notes(cookies['userID'], setNotes, cookies).then(() => console.log("Notes loaded"));
+        getNotes(cookies['userID'], setNotes, cookies).then(() => console.log("Notes loaded"));
     }, [cookies]);
     const {t: translation} = useLanguage();
+    const filteredNotes = notes.filter(note =>
+        note.props.children[0].props.children[1].props.children.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        note.props.children[1].props.dangerouslySetInnerHTML.__html.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     return (
         <>
             {TopBarAndSideMenu()}
             <div className={"notes-main-content"}>
                 <div className={"notes-search-input"}>
                     <img src={findicon} alt="finc-icon" title={"Znajdz"}/>
-                    <input type={"text"} name={"find"} className={"home-p"} placeholder={translation.Notes.find}/>
+                    <input type={"text"}
+                           name={"find"}
+                           className={"home-p"}
+                           placeholder={translation.Notes.find}
+                           onChange={event => setSearchTerm(event.target.value)}
+                    />
                 </div>
-                <div className={"note"} title={"Dodaj notatke"}  onClick={() => {
-                    if(document.getElementsByClassName("add-note-window")[0].style.display === "none") {
+                <div className={"note"} title={"Dodaj notatke"} onClick={() => {
+                    if (document.getElementsByClassName("add-note-window")[0].style.display === "none") {
                         document.getElementsByClassName("add-note-window")[0].style.display = "block";
                     } else {
                         document.getElementsByClassName("add-note-window")[0].style.display = "none";
@@ -136,17 +165,17 @@ const Notes = () => {
                         <img src={addicon} alt="add-note-icon"/>
                     </div>
                 </div>
-                {notes}
+                {filteredNotes}
                 <div className={"note-window"}>
                     <div className={"note-window-top-bar"}>
                         <div className={"note-window-top-bar-title"}>
                             Title
                         </div>
-                        <div className={"note-window-top-bar-modify"} onClick={()=>{
-                            if(document.getElementsByClassName("modify-note-window")[0].style.display === "none") {
+                        <div className={"note-window-top-bar-modify"} onClick={() => {
+                            if (document.getElementsByClassName("modify-note-window")[0].style.display === "none") {
                                 document.getElementsByClassName("modify-note-window")[0].style.display = "block";
-                                document.getElementById("modify-note-window-title").value = document.getElementsByClassName("note-window-top-bar-title")[0].textContent;
-                                document.getElementById("modify-note-window-text").value = document.getElementsByClassName("note-window-text")[0].textContent;
+                                document.getElementById("modify-note-window-title").value = document.getElementsByClassName("note-window-top-bar-title")[0].innerHTML;
+                                document.getElementById("modify-note-window-text").value = document.getElementsByClassName("note-window-text")[0].innerHTML.replace(new RegExp("<br>", "g"), "\n");
                             } else {
                                 document.getElementsByClassName("modify-note-window")[0].style.display = "none";
                             }
@@ -176,21 +205,25 @@ const Notes = () => {
                         </textarea>
                     </div>
                     <div className={"add-note-window-bottom-bar"}>
-                        <div className={"add-note-window-bottom-bar-item add-note-window-bottom-bar-item-cancel"} onClick={() =>{
-                            document.getElementsByClassName("cancel-window")[0].style.display = "block";
-                        }}>
+                        <div className={"add-note-window-bottom-bar-item add-note-window-bottom-bar-item-cancel"}
+                             onClick={() => {
+                                 document.getElementsByClassName("cancel-window")[0].style.display = "block";
+                             }}>
                             Anuluj
                         </div>
-                        <div className={"add-note-window-bottom-bar-item add-note-window-bottom-bar-item-add"} onClick={()=>{
-                            if(document.getElementById("add-note-window-title").value === "" || document.getElementById("add-note-window-text").value === "") {
-                                alert("Wypełnij wszystkie pola!");
-                                return;
-                            }
-                            add_note(cookies['userID'], document.getElementById("add-note-window-title").value, document.getElementById("add-note-window-text").value);
-                            document.getElementsByClassName("add-note-window")[0].style.display = "none";
-                            document.getElementById("add-note-window-title").value = "";
-                            document.getElementById("add-note-window-text").value = "";
-                        }}>
+                        <div className={"add-note-window-bottom-bar-item add-note-window-bottom-bar-item-add"}
+                             onClick={() => {
+                                 document.getElementsByClassName("add-note-window-bottom-bar-item-add")[0].onclick = () => {
+                                     if (document.getElementById("add-note-window-title").value === "" || document.getElementById("add-note-window-text").value === "") {
+                                         alert("Wypełnij wszystkie pola!");
+                                         return;
+                                     }
+                                     addNote(cookies['userID'], document.getElementById("add-note-window-title").value, document.getElementById("add-note-window-text").value, setNotes, cookies);
+                                     document.getElementsByClassName("add-note-window")[0].style.display = "none";
+                                     document.getElementById("add-note-window-title").value = "";
+                                     document.getElementById("add-note-window-text").value = "";
+                                 };
+                             }}>
                             Dodaj
                         </div>
                     </div>
@@ -202,7 +235,7 @@ const Notes = () => {
                         czy chcesz anulować?
                     </div>
                     <div className={"cancel-window-bottom-bar"}>
-                        <div className={"cancel-window-bottom-bar-item cancel-window-bottom-bar-yes"} onClick={()=>{
+                        <div className={"cancel-window-bottom-bar-item cancel-window-bottom-bar-yes"} onClick={() => {
                             document.getElementsByClassName("add-note-window")[0].style.display = "none";
                             document.getElementById("add-note-window-title").value = "";
                             document.getElementById("add-note-window-text").value = "";
@@ -214,7 +247,7 @@ const Notes = () => {
                         }}>
                             Tak
                         </div>
-                        <div className={"cancel-window-bottom-bar-item cancel-window-bottom-bar-no"} onClick={()=>{
+                        <div className={"cancel-window-bottom-bar-item cancel-window-bottom-bar-no"} onClick={() => {
                             document.getElementsByClassName("cancel-window")[0].style.display = "none";
                         }}>
                             Nie
@@ -239,10 +272,10 @@ const Notes = () => {
                         <div className={"modify-note-window-top-bar-text"}>
                             Modyfikuj notatke
                         </div>
-                        <div className={"modify-note-window-top-bar-delete"} onClick={()=>{
+                        <div className={"modify-note-window-top-bar-delete"} onClick={() => {
                             document.getElementsByClassName("modify-note-window")[0].style.display = "none";
                             document.getElementsByClassName("note-window")[0].style.display = "none";
-                            remove_note(currentNoteId, cookies);
+                            removeNote(currentNoteId, cookies, setNotes);
                         }}>
                             <img src={deleteicon} alt="delete-icon" title={"Usuń"}/>
                         </div>
@@ -254,23 +287,27 @@ const Notes = () => {
                         </textarea>
                     </div>
                     <div className={"modify-note-window-bottom-bar"}>
-                        <div className={"modify-note-window-bottom-bar-item modify-note-window-bottom-bar-item-cancel"} onClick={()=>{
-                            document.getElementsByClassName("cancel-window")[0].style.display = "block";
-                        }}>
+                        <div className={"modify-note-window-bottom-bar-item modify-note-window-bottom-bar-item-cancel"}
+                             onClick={() => {
+                                 document.getElementsByClassName("cancel-window")[0].style.display = "block";
+                             }}>
                             Anuluj
                         </div>
-                        <div className={"modify-note-window-bottom-bar-item modify-note-window-bottom-bar-item-save"} onClick={() =>{
-                            if(document.getElementById("modify-note-window-title").value === "" || document.getElementById("modify-note-window-text").value === "") {
-                                alert("Wypełnij wszystkie pola!");
-                                return;
-                            }
-                            modify_note(cookies['userID'], currentNoteId, document.getElementById("modify-note-window-title").value, document.getElementById("modify-note-window-text").value);
-                            document.getElementsByClassName("modify-note-window")[0].style.display = "none";
-                            document.getElementsByClassName("note-window")[0].style.display = "none";
-                            document.getElementById("modify-note-window-title").value = "";
-                            document.getElementById("modify-note-window-text").value = "";
+                        <div className={"modify-note-window-bottom-bar-item modify-note-window-bottom-bar-item-save"}
+                             onClick={() => {
+                                 if (document.getElementById("modify-note-window-title").value === "" || document.getElementById("modify-note-window-text").value === "") {
+                                     alert("Wypełnij wszystkie pola!");
+                                     return;
+                                 }
+                                 modifyNote(cookies['userID'], currentNoteId, document.getElementById("modify-note-window-title").value, document.getElementById("modify-note-window-text").value, setNotes).then(() => {
+                                     getNotes(cookies['userID'], setNotes, cookies);
+                                 });
+                                 document.getElementsByClassName("modify-note-window")[0].style.display = "none";
+                                 document.getElementsByClassName("note-window")[0].style.display = "none";
+                                 document.getElementById("modify-note-window-title").value = "";
+                                 document.getElementById("modify-note-window-text").value = "";
 
-                        }}>
+                             }}>
                             Zapisz
                         </div>
                     </div>
