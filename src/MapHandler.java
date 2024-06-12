460b8f4a45c40c7508630baeb185d20684ce2810
Juliusz Zolich <245971@edu.p.lodz.pl>
2024-06-12 21:16:35 +0200

    Dodanie obsługi mapy z poziomu http

--- src/MapHandler.java
+++ src/MapHandler.java
@@ -1,21 +1,52 @@
+import DBUtils.DBMap;
+
+import Utils.Utils;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.concurrent.ExecutionException;
 
 public class MapHandler implements HttpHandler {
-    @Override
-    public void handle(HttpExchange t) throws IOException {
-        String response = "XDDDD";
-        System.out.println("Request received");
-        t.sendResponseHeaders(200, response.length());
-        InputStream is = t.getRequestBody();
+
+    private void getMap(HttpExchange t) throws IOException, ExecutionException, InterruptedException {
         OutputStream os = t.getResponseBody();
-        System.out.println(new String(is.readAllBytes()));
+        String BuildingID = t.getRequestHeaders().get("BuildingID").get(0);
+        String response = DBMap.getMap(BuildingID);
+        t.sendResponseHeaders(200, response.getBytes().length);
+        System.out.println(response.getBytes().length);
         os.write(response.getBytes());
         os.close();
-        is.close();
+    }
+    private void getMapList(HttpExchange t) throws IOException {
+        OutputStream os = t.getResponseBody();
+        String response = DBMap.getMapList();
+        t.sendResponseHeaders(200, response.getBytes().length);
+        os.write(response.getBytes());
+        os.close();
+    }
+
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+        if (Utils.handleCORS(t)) return;
+        System.out.println("Map request received");
+        t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        switch (t.getRequestHeaders().get("Action-Type").get(0)) {
+            case "GET":
+                try {
+                    getMap(t);
+                } catch (ExecutionException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "LIST":
+                getMapList(t);
+                break;
+            default:
+                System.out.println("Invalid request: " + t.getRequestHeaders().get("Action-Type").get(0));
+                t.sendResponseHeaders(400, 0);
+                break;
+        }
     }
 }
