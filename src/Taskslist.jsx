ab76950c49c0d4139f1abf19850a0996b7e6cb64
Sebastian <245787@edu.p.lodz.pl>
2024-06-13 18:19:59 +0200

    Dodanie okienek do strony lista zadań

--- src/Taskslist.jsx
+++ src/Taskslist.jsx
@@ -135,6 +135,167 @@ const Tasklist = () => {
     return (
         <>
             {TopBarAndSideMenu()}
+            <div className={"task-window"}>
+                <div className={"task-window-top-bar"}>
+                    <div className={"task-window-top-bar-title"}>
+                        Title
+                    </div>
+                    <div className={"task-window-top-bar-modify"}>
+                        <img src={"./src/assets/modify.png"} alt="modify-icon" title={"Modyfikuj"}/>
+                    </div>
+                </div>
+                <div className={"task-window-text"}>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+                        dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+                        dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+                        dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+                        dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+                <div className={"task-window-bottom-bar"}>
+                    <div className={"task-window-bottom-bar-close"}>
+                        Zamknij
+                    </div>
+                </div>
+            </div>
+            <div className={"add-task-window"}>
+                <div className={"add-task-window-top-bar"}>
+                    Dodaj notatke
+                </div>
+                <div className={"add-task-window-middle-content"}>
+                    <input type="text" placeholder={"Wpisz tytuł notatki"}/>
+                    <textarea id={"add-task-window-text"} name={"task-content"} placeholder={"Wpisz treść notatki"}>
+                        </textarea>
+                </div>
+                <div className={"add-task-window-bottom-bar"}>
+                    <div className={"add-task-window-bottom-bar-item add-task-window-bottom-bar-item-cancel"}>
+                        Anuluj
+                    </div>
+                    <div className={"add-task-window-bottom-bar-item add-task-window-bottom-bar-item-add"}>
+                        Dodaj
+                    </div>
+                </div>
+            </div>
+            <div className={"cancel-window-task"}>
+                <div className={"cancel-window-task-text"}>
+                    Niezapisane zmiany <span
+                    className={"cancel-window-task-text-decoration"}>nie zostaną zachowane</span>,
+                    czy chcesz anulować?
+                </div>
+                <div className={"cancel-window-bottom-bar"}>
+                    <div className={"cancel-window-task-bottom-bar-item cancel-window-task-bottom-bar-yes"}>
+                        Tak
+                    </div>
+                    <div className={"cancel-window-task-bottom-bar-item cancel-window-task-bottom-bar-no"}>
+                        Nie
+                    </div>
+                </div>
+            </div>
+            <div className={"delete-window-task"}>
+                <div className={"delete-window-task-text"}>
+                    Czy napewno chcesz <span className={"delete-window-task-text-decoration"}>usunąć</span> notatke?
+                </div>
+                <div className={"delete-window-task-task-bottom-bar"}>
+                    <div className={"delete-window-task-bottom-bar-item delete-window-task-bottom-bar-delete"}>
+                        Usuń
+                    </div>
+                    <div className={"delete-window-task-bottom-bar-item delete-window-task-bottom-bar-keep"}>
+                        Zachowaj
+                    </div>
+                </div>
+            </div>
+            <div className={"modify-task-window"}>
+                <div className={"modify-task-window-top-bar"}>
+                    <div className={"modify-task-window-top-bar-text"}>
+                        Modyfikuj notatke
+                    </div>
+                    <div className={"modify-task-window-top-bar-delete"}>
+                        <img src={"./src/assets/bin.png"} alt="delete-icon" title={"Usuń"}/>
+                    </div>
+                </div>
+                <div className={"modify-task-window-middle-content"}>
+                    <input type="text" placeholder={"Wpisz tytuł notatki"}/>
+                    <textarea id={"modify-task-window-text"} name={"task-content"}
+                              placeholder={"Wpisz treść notatki"}>
+                        </textarea>
+                </div>
+                <div className={"modify-task-window-bottom-bar"}>
+                    <div className={"modify-task-window-bottom-bar-item modify-task-window-bottom-bar-item-cancel"}>
+                        Anuluj
+                    </div>
+                    <div className={"modify-task-window-bottom-bar-item modify-task-window-bottom-bar-item-save"}>
+                        Zapisz
+                    </div>
+                </div>
+            </div>
             <div className={"tasks-list-left-content-open-menu"} onClick={() => {OpenCloseTasksMenu(isOpenMenu);if(isOpenMenu === 1) isOpenMenu = 0;else isOpenMenu = 1}}>
                 <img className={"tasks-list-left-content-open-menu-img"} src={openmenu} alt="open-menu-icon"/>
             </div>
