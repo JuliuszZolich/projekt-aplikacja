eae8fdc3bcb77bae6452fdadffd7de1672c014d6
Juliusz Zolich <245971@edu.p.lodz.pl>
2024-06-16 16:49:01 +0200

    Rozszerzenie funkcjonalności "przedmiotów"

--- src/SubjectsFunctions.jsx
+++ src/SubjectsFunctions.jsx
@@ -1,17 +1,26 @@
 export function changeScenery(i){
     if(i === 1){
         document.getElementById("marks").style.display = 'none';
+        document.getElementById("exercises").style.display = 'none';
         document.getElementById("announcements").style.display = 'none';
         document.getElementById("lectures").style.display = 'block';
     }
     else if(i === 2){
         document.getElementById("marks").style.display = 'none';
+        document.getElementById("announcements").style.display = 'none';
+        document.getElementById("lectures").style.display = 'none';
+        document.getElementById("exercises").style.display = 'block';
+    }
+    else if(i === 3){
+        document.getElementById("marks").style.display = 'none';
         document.getElementById("announcements").style.display = 'block';
         document.getElementById("lectures").style.display = 'none';
+        document.getElementById("exercises").style.display = 'none';
     }
     else{
         document.getElementById("marks").style.display = 'block';
         document.getElementById("announcements").style.display = 'none';
         document.getElementById("lectures").style.display = 'none';
+        document.getElementById("exercises").style.display = 'none';
     }
 }
